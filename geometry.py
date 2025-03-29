223bef6de6294ab699c63323d4757f2ed0bc0ef5
unknown <uwumina56@gmail.com>
2025-03-30 01:12:05 +0500

    Added calculate_perimeter function

--- geometry.py
+++ geometry.py
@@ -6,3 +6,12 @@ def calculate_area(shape, **params):
         return pi * (params["radius"] ** 2)
     else:
         raise ValueError("Unsupported shape")
+
+def calculate_perimeter(shape, **params):
+    if shape == "rectangle":
+        return 2 * (params["width"] + params["height"])
+    elif shape == "circle":
+        from math import pi
+        return 2 * pi * params["radius"]
+    else:
+        raise ValueError("Unsupported shape")
